f11dcec3cb15dfe01f0202a0ee22899bf4a6a798
Mac <mac@MusfiQs-MacBook-Air.local>
2024-07-09 13:50:20 +0600

    explore our menu component done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,11 @@ const Header = () => {
     <div className="header">
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde assumenda, doloremque molestias, nemo rerum corporis sequi adipisci atque, fugiat repellendus velit doloribus quisquam iusto architecto.</p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde
+          assumenda, doloremque molestias, nemo rerum corporis sequi adipisci
+          atque, fugiat repellendus velit doloribus quisquam iusto architecto.
+        </p>
         <button>View Menu</button>
       </div>
     </div>
