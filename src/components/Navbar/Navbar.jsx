6db6de7039ca4a8ecdb19a528543e20dfbbac82f
Mac <mac@MusfiQs-MacBook-Air.local>
2024-07-10 10:03:02 +0600

    cart page done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 import "./Navbar.css";
 
 const Navbar = ({ setShowLogin }) => {
   const [menuBorder, setMenuBorder] = useState("home");
+  const { getTotalCartAmount } = useContext(StoreContext);
 
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to="/">
+        {" "}
+        <img src={assets.logo} alt="" className="logo" />{" "}
+      </Link>
       <ul className="navbar-menu">
         <Link
           onClick={() => setMenuBorder("home")}
@@ -34,8 +39,11 @@ const Navbar = ({ setShowLogin }) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search icon" />
         <div className="navbar-basket-icon">
-          <img src={assets.basket_icon} alt="basket icon" />
-          <div className="dot"></div>
+          <Link to="/cart">
+            {" "}
+            <img src={assets.basket_icon} alt="basket icon" />{" "}
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
         <button onClick={() => setShowLogin(true)}>Sign In</button>
       </div>
